0a6820a861d3dacd718a60900e8e4cdeec47a98b
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2026-05-26 14:55:25 +0700

    add env

--- springboot-be-ticketonline/src/main/java/ticket_online/ticket_online/TicketOnlineApplication.java
+++ springboot-be-ticketonline/src/main/java/ticket_online/ticket_online/TicketOnlineApplication.java
@@ -1,5 +1,6 @@
 package ticket_online.ticket_online;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //@EnableAsync
 public class TicketOnlineApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(TicketOnlineApplication.class, args);
 	}
 }
